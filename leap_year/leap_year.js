17175568141974666b54bf7d1de9870681994ca9
Nagy Máté <morsakk.mate@gmail.com>
2024-09-21 17:25:08 +0200

    3/3

---
+++ leap_year/leap_year.js
@@ -0,0 +1,19 @@
+window.onload = function()
+{
+    let button = document.getElementById("button");
+
+    button.onclick = function Calculate()
+    {
+        let year = Math.round(parseFloat(document.getElementById("year").value));
+        let out = document.getElementById("output");
+        if(isNaN(year)){
+            alert("Hibás adat! ");
+        }else{
+            if(year % 4 == 0){
+                out.innerHTML = "Szökőév! ";
+            }else{
+                out.innerHTML = "Nem szökőév!";
+            }
+        }
+    }
+}
