c7050385811ce10dea8c53fc71d12fcf3561b352
Nagy Máté <b1nagmat@vasvari.org>
2024-09-23 11:35:13 +0200

    8/8

---
+++ for_loop_04/for_loop_04.js
@@ -0,0 +1,19 @@
+window.onload = function()
+{
+    let button = document.getElementById("button");
+    let out = document.getElementById("output");
+
+    button.onclick = function Do()
+    {
+        out.innerHTML = "";
+        let text = document.getElementById("input").value;
+        if(text == ""){
+            alert("Adjon meg egy szöveget! ");
+        }else{
+            
+            for(let i  = 0; i < text.length; i++){
+                out.innerHTML += `<br>${text[i]}`;
+            }
+        }
+    }
+}
